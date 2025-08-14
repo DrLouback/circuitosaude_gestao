215c682c36c950d8a7294313e64ed9963f418158
DrLouback <drloubackmateus@gmail.com>
2025-08-14 15:00:00 -0300

    Mudanças na formatação para real e correção valor por titulo

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -75,8 +75,18 @@ pivot_alunos = alunos.groupby('mes')['cliente'].nunique().reset_index()
 pivot_alunos = pivot_alunos.set_index('mes').T
 pivot_alunos.index = pd.Index(['Quantidade de alunos'])
 
+if unidade == 'MOK':
+    pivot_alunos.insert(0,1,128)
+    pivot_alunos.insert(1,2,135)
+    pivot_alunos.insert(2,3,128)
+
+if unidade == 'Shopping':
+    pivot_alunos.insert(0,1,158)
+    pivot_alunos.insert(1,2,167)
+    pivot_alunos.insert(2,3,133)
+
 st.write('Quantidade de alunos')
-st.dataframe(pivot_alunos)
+st.dataframe(pivot_alunos.fillna(''))
 
 # ------------------------------
 # CONTAS A PAGAR
@@ -85,6 +95,7 @@ st.divider()
 contas_pagar = pd.read_sql(f"SELECT * FROM contas_pagar WHERE unidade = '{unidade}'", engine)
 contas_pagar['titulo'] = contas_pagar['titulo'].str.replace(r"\s*\(.*?\)", "", regex=True).str.strip()
 contas_pagar['mes_pagamento'] = contas_pagar['mes_pagamento'].dropna().astype(int)
+
 
 pivot_contas_pagar = contas_pagar.pivot_table(
     index=[],
@@ -108,10 +119,39 @@ df_fluxo = pd.DataFrame(
     index=['Recebido', 'Saídas', 'Lucro'],
     columns=todos_meses
 )
+if unidade == 'MOK':
+    valores_mes1 = [37276.81, 39286.96, 2010.15]
+    valores_mes2 = [45128.51, 41974.49, 3154.02]
+    valores_mes3 = [47000.92, 40714.46, 6286.46]
+    # Adicionar o mês 3 na posição correta
+    df_fluxo.insert(0, 1, valores_mes1)
+    df_fluxo.insert(1, 2, valores_mes2)
+    df_fluxo.insert(2, 3, valores_mes3)
+
+if unidade == 'Shopping':
+    if 3 in df_fluxo.columns:
+        df_fluxo.drop(columns=[3], inplace=True)
+    valores_mes1 = [57495.43, 52927.32, 4568.11]
+    valores_mes2 = [55498.23, 53098.08, 2400.15]
+    valores_mes3 = [65290.42, 52374.75, 12915.67]
+    # Adicionar o mês 3 na posição correta
+    
+    df_fluxo.insert(0, 1, valores_mes1)
+    df_fluxo.insert(1, 2, valores_mes2)
+    df_fluxo.insert(2, 3, valores_mes3)
+
 df_fluxo['Total'] = df_fluxo.sum(axis=1)
 
 st.write('Fluxo Real de Entradas por mês')
-st.dataframe(df_fluxo)
+
+st.dataframe(df_fluxo.style.format(
+    lambda x: f"R$ {x:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+    if isinstance(x, (int, float)) and pd.notna(x)
+    else ""
+))
+
+
+    
 
 # ------------------------------
 # DIFERENÇA PERCENTUAL MÊS A MÊS
@@ -136,13 +176,46 @@ pivot_titulos.loc['Total'] = pivot_titulos.sum(axis=0)
 # ------------------------------
 # DIFERENÇA ABSOLUTA DE GASTOS
 # ------------------------------
-pivot_titulos_diff = pivot_titulos.drop('Total', errors='ignore').iloc[:, :-1].diff(axis=1).fillna(0)
-pivot_titulos_diff.loc['Total'] = pivot_titulos_diff.sum(axis=0)
+st.divider()
+contas_pagar = pd.read_sql(f"""
+SELECT
+    titulo_limpo,
+    mes_pagamento,
+    total_mes - LAG(total_mes) OVER (PARTITION BY titulo_limpo ORDER BY mes_pagamento) AS diferenca_mes_anterior
+FROM (
+    SELECT
+        REGEXP_REPLACE(titulo, '\\s*\\(.*\\)', '', 'g') AS titulo_limpo,
+        mes_pagamento,
+        unidade,
+        SUM(valor) AS total_mes
+    FROM contas_pagar
+    GROUP BY REGEXP_REPLACE(titulo, '\\s*\\(.*\\)', '', 'g'), mes_pagamento, unidade
+) AS agrupado
+WHERE 
+  unidade = '{unidade}'
+ORDER BY mes_pagamento, titulo_limpo, unidade;
+ """, engine)
 
-st.write('Diferença absoluta de gastos por título e mês (mês atual - mês anterior)')
-st.dataframe(pivot_titulos_diff)
+df_contas_pagar = contas_pagar.pivot(
+    index='titulo_limpo', 
+    columns='mes_pagamento', 
+    values='diferenca_mes_anterior'
+)
 
+# Opcional: ordenar colunas pelo mês
+df_pivot = df_contas_pagar.sort_index(axis=1)
 
+# Opcional: resetar índice para virar coluna normal
+df_pivot = df_contas_pagar.reset_index()
+
+cols_numericas = df_pivot.select_dtypes(include=["number"]).columns
+
+df_faturamento = df_pivot.style.format(
+    subset=cols_numericas,
+    formatter=lambda x: f"R$ {x:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+)
+
+st.dataframe(df_faturamento)
 # ------------------------------
 # Faturamentos
 # ------------------------------
@@ -183,10 +256,14 @@ df_faturamento = pd.DataFrame(
     index=['Recebido', 'Saídas', 'Lucro'],
     columns=todos_meses
 )
-df_faturamento['Total'] = df_faturamento.sum(axis=1)
 
+df_faturamento['Total'] = df_faturamento.sum(axis=1)
 st.write('Faturamento')
-st.dataframe(df_faturamento)
+st.dataframe(df_faturamento.style.format(
+    lambda x: f"R$ {x:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+    if isinstance(x, (int, float)) and pd.notna(x)
+    else ""
+))
 
 print("Alunos por mês considerando alunos pagantes")
 print(pivot_alunos)
@@ -195,5 +272,5 @@ print(df_fluxo)
 print("Faturamento considerando entradas como mês de venda")
 print(df_faturamento)
 print("Diferença de gastos por títulos em relação ao mês anterior")
-print(pivot_titulos_diff)
+
 
